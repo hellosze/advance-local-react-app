3e7321b4b82c94bd89149365ead2906c9e2128db
hellosze <sze@hello-sze.com>
2019-03-15 01:59:41 -0400

    Add React Proptypes

--- src/AdUnit.js
+++ src/AdUnit.js
@@ -1,15 +1,56 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
 class AdUnit extends React.Component {
-    
-    render() {
-        return (
-        <div className="shopping-list">
-            <h1>Shopping List for {this.props.name}</h1>
-            <ul>
-            <li>Instagram</li>
-            <li>WhatsApp</li>
-            <li>Oculus</li>
-            </ul>
-        </div>
-        );
+    constructor(props) {
+      super(props);
+      this.adSize = this.props.size;
+      this.slotName = this.props.slotName;
+      this.sizeTargetingValue = this.adSize[0]+"x"+this.adSize[1];
+      this.slotKeyValues = this.props.slotKeyValues;
+      this.slotKeyValues["size"] = this.sizeTargetingValue
+      this.state = {
+        adSize: this.adSize,
+        slotKeyValues: this.slotKeyValues,
+        slotName: this.slotName
+      };
     }
-}
+  
+    componentDidMount(){
+      const slotName = this.slotName;
+      const adSize = this.adSize;
+      const slotKeyValues = this.slotKeyValues;
+      
+      window.googletag.cmd.push(function() {
+        var adSlot = window.googletag.defineSlot('/344101295/SI/www.silive.com/news/index.ssf', adSize, slotName);
+        if(typeof slotKeyValues === 'object'){
+          Object.keys(slotKeyValues).forEach(function(element){
+            adSlot.setTargeting(element,slotKeyValues[element]);
+          });
+        }
+        adSlot.addService(window.googletag.pubads());
+        window.googletag.display(slotName);
+        window.googletag.pubads().setTargeting("tpid",localStorage.getItem("tpid"));
+        window.googletag.enableServices();
+      });
+    }
+  
+    render() {
+      const slotName = this.slotName;
+      return (
+        <div className="gpt-ads" id={slotName}></div>
+      )
+    }
+  }
+
+  AdUnit.defaultProps = {
+      size: [300, 250],
+      slotName: "default-ad-slot"
+  }
+
+  AdUnit.propTypes = {
+    size: PropTypes.object,
+    slotName: PropTypes.string
+  };
+  
+  export default AdUnit
--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
+import AdUnit from './AdUnit'
 
 const LOTAME_API = 'https://ad.crwdcntrl.net/5/c=931/pe=y/';
-
-class AdUnit extends React.Component {
-  constructor(props) {
-    super(props);
-    this.adSize = this.props.size;
-    this.slotName = this.props.slotName;
-    this.sizeTargetingValue = this.adSize[0]+"x"+this.adSize[1];
-    this.slotKeyValues = this.props.slotKeyValues;
-    this.slotKeyValues["size"] = this.sizeTargetingValue
-    this.state = {
-      adSize: this.adSize,
-      slotKeyValues: this.slotKeyValues,
-      slotName: this.slotName
-    };
-  }
-
-  componentDidMount(){
-    const slotName = this.slotName;
-    const adSize = this.adSize;
-    const slotKeyValues = this.slotKeyValues;
-    
-    window.googletag.cmd.push(function() {
-      var adSlot = window.googletag.defineSlot('/344101295/SI/www.silive.com/news/index.ssf', adSize, slotName);
-      if(typeof slotKeyValues === 'object'){
-        Object.keys(slotKeyValues).forEach(function(element){
-          adSlot.setTargeting(element,slotKeyValues[element]);
-        });
-      }
-      adSlot.addService(window.googletag.pubads());
-      window.googletag.display(slotName);
-      window.googletag.pubads().setTargeting("tpid",localStorage.getItem("tpid"));
-      window.googletag.enableServices();
-    });
-  }
-
-  render() {
-    const slotName = this.slotName;
-    return (
-      <div className="gpt-ads" id={slotName}></div>
-    )
-  }
-}
 
 class App extends Component {
   constructor () {
@@ -139,9 +98,10 @@ class App extends Component {
           <div>{slot2KeyValues}</div>
         </div>
 
-        <button className='button btn-primary' onClick={this.handleClick}>Randomize Key values for Ads</button>
+        <button className='button btn-primary' onClick={this.handleClick}>Randomize Slot Key values for Ads</button>
       </div>
     )
   }
 }
+
 export default App
